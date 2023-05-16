f480ab5aea908fdf8425ede1c6e4ffbc3d8f3bca
Taseer <taseer.ahmadd@gmail.com>
2023-05-16 18:22:50 +0500

    Added typewriter effect

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
-const devName = document.getElementById('dev-name');
+const typewrite = document.getElementById('typewrite');
 const modal = new bootstrap.Modal(document.getElementById('notify-modal'));
 const modalLabel = document.getElementById('modal-label');
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+const typeWriter = new Typewriter(typewrite, {
+    loop: true,
+    delay: 100
+});
 
 window.onload = async () => {
-    const devNameChars = ['T', 'a', 's', 'e', 'e', 'r', ' ', 'A', 'h', 'm', 'a', 'd', ' ', '.'];
-    for (let i = 0; i < devNameChars.length; i++) {
-       typeWrite(devNameChars, i);
-       await sleep(350);
-    }
-
-    playFullStopAnimation();
+    const toWrite = ['Taseer Ahmad .', 'a Software Engineer .',
+                     ' Problem Solver .', ' Mobile Developer .', 
+                     ' Collaborater .', ' Coffee Lover .', 
+                     ' Game Night Aficionado .', ' Geek .', 
+                     ' Lifelong Leaner .', ' Friend ♥ .'];
+    typeWriter
+        .pauseFor(700)
+        .typeString(toWrite[0])
+        .pauseFor(2000)
+        .deleteChars(toWrite[0].length)
+        .typeString(toWrite[1])
+        .pauseFor(400)
+        .deleteChars(toWrite[1].length - 1)
+        .typeString(toWrite[2])
+        .pauseFor(400)
+        .deleteChars(toWrite[2].length)
+        .typeString(toWrite[3])
+        .pauseFor(400)
+        .deleteChars(toWrite[3].length)
+        .typeString(toWrite[4])
+        .pauseFor(400)
+        .deleteChars(toWrite[4].length)
+        .typeString(toWrite[5])
+        .pauseFor(400)
+        .deleteChars(toWrite[5].length)
+        .typeString(toWrite[6])
+        .pauseFor(400)
+        .deleteChars(toWrite[6].length)
+        .typeString(toWrite[7])
+        .pauseFor(400)
+        .deleteChars(toWrite[7].length)
+        .typeString(toWrite[8])
+        .pauseFor(400)
+        .deleteChars(toWrite[8].length)
+        .typeString(toWrite[9])
+        .pauseFor(400)
+        .deleteChars(toWrite[9].length + 1)
+        .pauseFor(1000)
+        .start();
 };
-
-function sleep(time) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, time || 1000);
-    });
-}
-
-function typeWrite(arr, index) {
-    devName.innerHTML = devName.innerHTML + arr[index];
-}
-
-function playFullStopAnimation() {
-    setInterval(() => {
-        const len = devName.innerHTML.length;
-        const fullStop = devName.innerHTML[len - 1];
-        if (fullStop === '.') {
-            devName.innerHTML = devName.innerHTML.slice(0, len - 1);
-        } else {
-            devName.innerHTML = devName.innerHTML + ' ' + '.';
-        }
-    }, 500);
-}
 
 function copyEmailAddress() {
     copyToClipboard('taseer.ahmadd@gmail.com');
